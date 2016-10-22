ad5ba71743031087d4dc8162ad48b12ed833fbee
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-10-22 02:15:46 +0000

    functionality works

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ var app = express();
 
 // app.set('port', (process.env.PORT || 5000));
 
+app.get("/api/whoami/", function(req, res) {
+    
+    console.log(req.headers["user-agent"]);
+    var requestHeaderObject = {};
+    requestHeaderObject.ipaddress = req.headers["x-forwarded-for"];
+    requestHeaderObject.language = req.headers["accept-language"];
+    requestHeaderObject.software = req.headers["user-agent"];
+    res.send(requestHeaderObject); 
+});
 
 
 // Catch-all error message
